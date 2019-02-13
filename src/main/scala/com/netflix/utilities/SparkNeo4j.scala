836ae926c58f3bf74dc80091fd3fbd605cff5be1
sankarb475 <sankarb475@gmail.com>
2019-02-13 20:36:51 +0530

    initial commit

---
+++ src/main/scala/com/netflix/utilities/SparkNeo4j.scala
@@ -0,0 +1,36 @@
+package com.netflix.utilities
+
+
+import org.neo4j.spark._
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+object SparkNeo4j {
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setAppName("APP_NAME")
+      .setMaster("local")
+      .set("spark.cassandra.connection.host", "localhost")
+      .set("spark.cassandra.auth.username", "")
+      .set("spark.cassandra.auth.password", "")
+
+    val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+
+    val sc: SparkContext = spark.sparkContext
+
+    val neo = Neo4j(sc)
+
+    val rdd = neo.cypher("MATCH (n:Person) RETURN id(n) as id ").loadRowRdd
+
+    //rdd.collect().foreach(println)
+
+    print("other stuff")
+    print(rdd.count)
+    print("after it")
+
+    print(rdd.first.schema.fieldNames)
+
+  }
+
+}
